078f6f4e7cd805df5a8bca9011fa35c4c002d680
KodingCore <kcorvais@gmail.com>
2023-05-25 10:30:25 +0000

    paint casi-finalized

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,28 @@
+const scene = document.getElementById("scene");
 let allCases = [];
+const inputColor = document.querySelector(".color input");
+const inputLignes = document.querySelector(".lignes input");
+const inputColonnes = document.querySelector(".colonnes input");
+const deleteBtn = document.querySelector("#deleteBtn");
+let mainRows = 10;
+let mainColumns = 10;
+let deleteMode = false;
 
-function gridCreator(){
-    const scene = document.getElementById("scene");
+function gridCreator(rows, cols){
+    
     let counterRow = 0;
     let counterCol = 0;
     
-    for(let nbrCase = 0; nbrCase < 100; nbrCase++){
+    scene.style.gridTemplateRows = "repeat(" + rows + ", 1fr)";
+    scene.style.gridTemplateColumns = "repeat(" + cols + ", 1fr)";
+    
+    for(let nbrCase = 0; nbrCase < parseInt(rows)*parseInt(cols); nbrCase++){
         let cases = document.createElement("div");
-        scene.style.height = "100vh";
+        scene.style.height = "86vh";
         scene.appendChild(cases);
         cases.style.border = "dotted 1px";
         counterCol++;
-        if(nbrCase % 10 === 0){
+        if(nbrCase % parseInt(cols) === 0){
             counterRow++;
             counterCol = 1;
         }
@@ -21,21 +32,72 @@ function gridCreator(){
     }
 }
 
-function selectCases(){
-    for(let box of allCases){
-        box.addEventListener("click", function(){
-            box.style.border = "solid 1px";
-        })
-    }
+let colorSelected = "#000";
+
+function setColorCases(){
+
+    addEventListener("click", function(e){
+        if(e.target.classList[0].slice(0, 3) === "box"){
+            if(deleteMode){
+                e.target.style.border = "dotted 1px";
+                e.target.style.backgroundColor = "#fff";
+            }else{
+                e.target.style.border = "solid 1px";
+                e.target.style.backgroundColor = colorSelected;
+            }
+        }
+    })
+
 }
 
 function selectColor(){
-    const inputColor = document.
-    "change"
+    inputColor.addEventListener("change", function(e){
+        colorSelected = e.target.value;
+    })
 }
 
+
+function gridSizing(){
+    inputLignes.addEventListener("change", function(e){
+        if(e.target.value > 100 || isNaN(e.target.value)){
+            alert("Vous devez entrer un nombre inférieur à 100");
+        }else{
+            mainRows = e.target.value;
+            allCases = [];
+            scene.innerHTML = "";
+            gridCreator(mainRows, mainColumns);
+        }
+        
+    })
+    
+    inputColonnes.addEventListener("change", function(e){
+        if(e.target.value > 100 || isNaN(e.target.value)){
+            alert("Vous devez entrer un nombre inférieur à 100");
+        }else{
+            mainColumns = e.target.value;
+            allCases = [];
+            scene.innerHTML = "";
+            gridCreator(mainRows, mainColumns);
+        }
+    })
+}
+
+function deleteBoxColor(){
+    deleteBtn.addEventListener("click", function(){
+        if(deleteMode){
+            deleteMode = false;
+        }else{
+            deleteMode = true;
+        }
+        
+    })
+}
+
+
 window.addEventListener("DOMContentLoaded", function(){
-    gridCreator();
-    selectCases();
+    gridCreator(mainRows, mainColumns);
+    gridSizing();
     selectColor();
+    setColorCases();
+    deleteBoxColor();
 });
